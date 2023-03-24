2ffd9b5a2b4e4d23c447ece7bd58acb6c0413746
Gabriel Santos <gabriel.santos@cbyk.com.br>
2023-03-23 22:47:01 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        // Passo 1
+        List<int> lista = new List<int> { 1, 2, 3, 4, 5 };
+
+        // Passo 2
+        Stack<int> pilha = new Stack<int>();
+        while (lista.Count > 0)
+        {
+            pilha.Push(lista[0]);
+            lista.RemoveAt(0);
+        }
+
+        // Passo 3
+        Queue<int> fila = new Queue<int>(pilha);
+
+        // Passo 4
+        lista.AddRange(new List<int> { 6, 7, 8, 9, 10 });
+
+        // Passo 5
+        pilha.Clear();
+        while (lista.Count > 0)
+        {
+            pilha.Push(lista[0]);
+            lista.RemoveAt(0);
+        }
+        while (pilha.Count > 0)
+        {
+            fila.Enqueue(pilha.Pop());
+        }
+
+        // Passo 6
+        foreach (int num in fila)
+        {
+            Console.Write(num + " ");
+        }
+    }
+}
